939473baca60640468a24749240434911f974393
Neha Ramchandani <74250901+nehawork@users.noreply.github.com>
2021-08-01 00:59:46 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-    System.out.println("Good morning!");    
+    System.out.println("Good morning!"); 
+    System.out.println("Added from github edit.."); 
   }
 }
